d2e3dfc0fb1265465e42025d793d16155a5a12c9
momu54 <96234201+momu54@users.noreply.github.com>
2023-04-22 12:44:07 +0800

    (add) [m.html, script.js, style-mobile.css] Add mobile page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-document
-	.querySelector('.Special-btn')
-	?.addEventListener(
-		'click',
-		() => (window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'),
-	);
+if (
+	/Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/.test(
+		window.navigator.userAgent
+	)
+) {
+	window.location.href = './m';
+}
