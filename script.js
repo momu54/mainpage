9aec3c8299a3b0993f4eabae14bf17b3662c7d86
momu54 <96234201+momu54@users.noreply.github.com>
2023-08-29 14:55:32 +0800

    feat: Add cursor background

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ if (
 ) {
 	window.location.href = './m';
 }
+
+document.addEventListener('mousemove', (event) => {
+	const cursor_element = document.getElementsByClassName('cursorbg');
+	cursor_element[0].style.left = event.clientX - 20 + 'px';
+	cursor_element[0].style.top = event.clientY - 20 + 'px';
+});
