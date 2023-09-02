8f2c124f88c56c0f552187cb66a25d0af3d7f0c9
momu54 <96234201+momu54@users.noreply.github.com>
2023-09-02 21:14:31 +0800

    feat: Add button effect
    fix: Fix cursor background show bug

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ if (
 	window.location.href = './m';
 }
 
+let ishoveronbtn = false;
+
 document.addEventListener('mousemove', (event) => {
 	const cursor_element = document.getElementsByClassName('cursorbg');
-	cursor_element[0].style.left = event.clientX - 20 + 'px';
-	cursor_element[0].style.top = event.clientY - 20 + 'px';
+	console.log(cursor_element[0].style.display);
+	if (!cursor_element[0].style.display) cursor_element[0].style.display = 'block';
+	cursor_element[0].style.left = event.clientX - (ishoveronbtn ? 10 : 20) + 'px';
+	cursor_element[0].style.top = event.clientY - (ishoveronbtn ? 10 : 20) + 'px';
+});
+
+const socialbtn = document.getElementsByClassName('socialbtn');
+[...socialbtn].forEach((element) => {
+	/** @type {HTMLLinkElement} */ (element).addEventListener('mouseenter', () => {
+		const cursor_element = document.getElementsByClassName('cursorbg');
+		cursor_element[0].style.height = '20px';
+		cursor_element[0].style.width = '20px';
+		ishoveronbtn = true;
+	});
+	/** @type {HTMLLinkElement} */ (element).addEventListener('mouseout', () => {
+		const cursor_element = document.getElementsByClassName('cursorbg');
+		cursor_element[0].style.height = '40px';
+		cursor_element[0].style.width = '40px';
+		ishoveronbtn = false;
+	});
 });
